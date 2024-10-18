14a81abf5b82e8b721d4e8b221bc0c17185c4937
sskkilm <sskkilm@naver.com>
2024-10-18 18:55:59 +0900

    feat: multi module init

---
+++ src/test/java/com/example/dropsaleplatform/DropSalePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dropsaleplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropSalePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
