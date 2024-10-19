3cecbc3be0c881d940a3be20f341dff0d5f8d3da
sskkilm <sskkilm@naver.com>
2024-10-19 19:06:52 +0900

    test: 상품 상세 정보 조회

--- product/src/test/java/com/example/product/application/ProductServiceTest.java
+++ product/src/test/java/com/example/product/application/ProductServiceTest.java
@@ -1,13 +1,18 @@
 package com.example.product.application;
 
+import com.example.product.domain.Money;
+import com.example.product.domain.Product;
+import com.example.product.dto.ProductDetails;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 
@@ -30,5 +35,28 @@ class ProductServiceTest {
         assertThrows(IllegalArgumentException.class,
                 // when
                 () -> productService.getProductDetails(1L));
+    }
+
+    @Test
+    void 상품_상세_정보_조회() {
+        //given
+        given(productRepository.findById(1L))
+                .willReturn(Optional.of(
+                        Product.builder()
+                                .id(1L)
+                                .name("name")
+                                .price(Money.of(new BigDecimal("10000")))
+                                .stockQuantity(100)
+                                .build()
+                ));
+
+        //when
+        ProductDetails productDetails = productService.getProductDetails(1L);
+
+        //then
+        assertEquals(1L, productDetails.productId());
+        assertEquals("name", productDetails.name());
+        assertEquals("10000", productDetails.price());
+        assertEquals(100, productDetails.stockQuantity());
     }
 }
