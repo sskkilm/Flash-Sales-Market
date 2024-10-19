99d8c4634566a5fbc5011fd79316b770129d2b67
sskkilm <sskkilm@naver.com>
2024-10-19 14:12:42 +0900

    feat: jpa auditing 설정 추가

---
+++ product/src/main/java/com/example/product/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.example.product.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
