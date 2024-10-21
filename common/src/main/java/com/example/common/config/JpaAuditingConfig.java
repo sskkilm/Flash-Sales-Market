f2f5682e9b9694633e3fab55a6363df59e1d3a5a
sskkilm <sskkilm@naver.com>
2024-10-21 14:29:00 +0900

    feat: 공통 모듈에 Auditing config 추가

--- common/src/main/java/com/example/common/config/JpaAuditingConfig.java
+++ common/src/main/java/com/example/common/config/JpaAuditingConfig.java
@@ -1,4 +1,4 @@
-package com.example.member.config;
+package com.example.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
