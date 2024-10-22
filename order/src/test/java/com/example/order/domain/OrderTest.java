3d0c545df6de59a0edcd5115797b097320feb47e
sskkilm <sskkilm@naver.com>
2024-10-22 13:22:41 +0900

    test: 회원의 주문이 맞는지, 취소될 수 있는 주문인지 확인

--- order/src/test/java/com/example/order/domain/OrderTest.java
+++ order/src/test/java/com/example/order/domain/OrderTest.java
@@ -2,8 +2,7 @@ package com.example.order.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class OrderTest {
 
@@ -21,5 +20,33 @@ class OrderTest {
         assertEquals(OrderStatus.ORDER_COMPLETED, order.getStatus());
         assertNull(order.getCreatedAt());
         assertNull(order.getUpdatedAt());
+    }
+
+    @Test
+    void 회원의_주문이_맞는지_확인() {
+        //given
+        Order order = Order.builder()
+                .memberId(1L)
+                .build();
+
+        //when
+        boolean orderNotOrderedBy = order.isNotOrderedBy(1L);
+
+        //then
+        assertFalse(orderNotOrderedBy);
+    }
+
+    @Test
+    void 취소될_수_있는_주문인지_확인() {
+        //given
+        Order order = Order.builder()
+                .status(OrderStatus.ORDER_COMPLETED)
+                .build();
+
+        //when
+        boolean canNotBeCanceled = order.canNotBeCanceled();
+
+        //then
+        assertFalse(canNotBeCanceled);
     }
 }
