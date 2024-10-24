461261d6064b69d4c93f8fdfd9c0327f370bf670
sskkilm <sskkilm@naver.com>
2024-10-24 20:08:24 +0900

    test: 장바구니 항목 수정 및 삭제 시 예외 상황 테스트

---
+++ member/src/test/java/com/example/member/application/CartItemServiceTest.java
@@ -0,0 +1,80 @@
+package com.example.member.application;
+
+import com.example.member.domain.CartItem;
+import com.example.member.dto.CartItemUpdateRequest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class CartItemServiceTest {
+
+    @InjectMocks
+    CartItemService cartItemService;
+
+    @Mock
+    CartItemRepository cartItemRepository;
+
+    @Test
+    void 장바구니_항목_수정_시_존재하지_않는_장바구니_항목이면_예외가_발생한다() {
+        //given
+        CartItemUpdateRequest request = new CartItemUpdateRequest(1);
+        given(cartItemRepository.findById(1L))
+                .willReturn(Optional.empty());
+        //then
+        assertThrows(IllegalArgumentException.class,
+                // when
+                () -> cartItemService.update(1L, 1L, request));
+    }
+
+    @Test
+    void 장바구니_항목_수정_시_회원_정보가_다르면_예외가_발생한다() {
+        //given
+        CartItemUpdateRequest request = new CartItemUpdateRequest(1);
+        given(cartItemRepository.findById(1L))
+                .willReturn(Optional.of(
+                        CartItem.builder()
+                                .id(1L)
+                                .memberId(2L)
+                                .build()
+                ));
+        //then
+        assertThrows(IllegalArgumentException.class,
+                // when
+                () -> cartItemService.update(1L, 1L, request));
+    }
+
+    @Test
+    void 장바구니_항목_삭제_시_존재하지_않는_장바구니_항목이면_예외가_발생한다() {
+        //given
+        given(cartItemRepository.findById(1L))
+                .willReturn(Optional.empty());
+        //then
+        assertThrows(IllegalArgumentException.class,
+                // when
+                () -> cartItemService.delete(1L, 1L));
+    }
+
+    @Test
+    void 장바구니_항목_삭제_시_회원_정보가_다르면_예외가_발생한다() {
+        //given
+        given(cartItemRepository.findById(1L))
+                .willReturn(Optional.of(
+                        CartItem.builder()
+                                .id(1L)
+                                .memberId(2L)
+                                .build()
+                ));
+        //then
+        assertThrows(IllegalArgumentException.class,
+                // when
+                () -> cartItemService.delete(1L, 1L));
+    }
+}
