62899a8bd5aa5ef2baae373f84d7e59a4062fde6
sskkilm <sskkilm@naver.com>
2024-11-05 19:56:23 +0900

    feat: 결제 도메인 전역 예외 핸들러 추가

---
+++ payment/src/main/java/com/example/payment/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.payment.exception;
+
+import com.example.payment.exception.error.ErrorResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(PaymentServiceException.class)
+    public ResponseEntity<?> handlePaymentServiceException(PaymentServiceException e) {
+        return ResponseEntity.badRequest().body(
+                new ErrorResponse(e.getErrorCode(), e.getErrorCode().getMessage())
+        );
+    }
+}
