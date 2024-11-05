d3f16530ec93d097ef50c5949847f2eac0fdae56
sskkilm <sskkilm@naver.com>
2024-11-05 19:50:05 +0900

    feat: 결제 도메인 ErrorCode 추가 - PAYMENT_INFO_ALREADY_EXIST

--- payment/src/main/java/com/example/payment/exception/error/ErrorCode.java
+++ payment/src/main/java/com/example/payment/exception/error/ErrorCode.java
@@ -1,12 +1,14 @@
 package com.example.payment.exception.error;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
     INVALID_ORDER_INFO("유효하지 않은 주문 정보입니다."),
-    PAYMENT_FAILED("결제 실패")
-    ;
+    PAYMENT_INFO_ALREADY_EXIST("결제 정보가 이미 존재합니다,"),
+    PAYMENT_FAILED("결제에 실패했습니다");
 
     private final String message;
 }
