0b7dfe4a176ebb2a1511b77c1106bb272054febb
LSG <sskkilm@naver.com>
2024-11-16 03:33:48 +0900

    feat: 카프카 프로듀서 설정

--- payment/src/main/java/com/example/payment/config/KafkaProducerConfig.java
+++ payment/src/main/java/com/example/payment/config/KafkaProducerConfig.java
@@ -14,7 +14,7 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
-    private static final String KAFKA_BROKER_URL = "localhost:9092";
+    private static final String KAFKA_BROKER_URL = "localhost:9094";
 
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
